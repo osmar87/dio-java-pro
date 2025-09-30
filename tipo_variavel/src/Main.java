111ebcbeb202a8c591cb0f7386eb813296b3f748
osmar gomes <osmar.dev87@gmail.com>
2025-09-30 08:10:43 -0300

    classes java

---
+++ tipo_variavel/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        short numCurto = 1;
+        int num1 = numCurto;
+        short numCurto2 = (short)num1;
+        num1 = 10;
+        final int NUMERO = 32;
+       final double VALOR_DE_PI = 3.14;
+
+       System.out.println(VALOR_DE_PI);
+
+       System.out.println(numCurto2);
+
+       System.out.println(NUMERO);
+
+    }
+}
---
+++ tipo_variavel/src/tipos_variaveis/TiposVariaveis.java
@@ -0,0 +1,19 @@
+package tipos_variaveis;
+
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        short numCurto = 1;
+        int num1 = numCurto;
+        short numCurto2 = (short)num1;
+        num1 = 10;
+        final int NUMERO = 32;
+       final double VALOR_DE_PI = 3.14;
+
+       System.out.println(VALOR_DE_PI);
+
+       System.out.println(numCurto2);
+
+       System.out.println(NUMERO);
+
+    }
+}
